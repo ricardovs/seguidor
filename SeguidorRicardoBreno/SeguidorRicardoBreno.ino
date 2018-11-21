d302d329eb72bce33e4dcec2edc53d7dfec0454b
erusbot <ultrabots3>
2018-11-21 18:40:24 -0200

    Update SeguidorRicardoBreno.ino
    Codigo adiocionado

--- SeguidorRicardoBreno/SeguidorRicardoBreno.ino
+++ SeguidorRicardoBreno/SeguidorRicardoBreno.ino
@@ -1,9 +1,19 @@
+#define pS1 A1
+#define pS2 A2
+#define pS3 A3
+#define pS4 A4
+#define pS5 A5
+#include "Sensor.h"
+
+char estado = 0;
+Sensor s1(A1, 55); 
 void setup() {
   // put your setup code here, to run once:
-
+  //Sensores
+    Serial.begin(9500);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  s1.ereload();
 }
